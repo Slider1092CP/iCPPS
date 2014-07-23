fdf26615a5ea379b0e9d9cb4cd07be2c234acdad
Slider1092CP <slider1092cp@gmail.com>
2014-07-23 14:36:25 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$config["ADDRESS"] = 0;//Address to listen on. Use 0 for all
+$config["PORT"] = 9875;
+$config["max_clients"] = 300;
+$config["DEBUG"] = true;
+$config["RAW_STR"] = true;
+$config["RAW_SEPERATOR"] = "%";
+$config["MAX_CLIENTS"] = 300;
+$config["USE_SEND_HANDLER"] = true; //Uses
+$config["SEND_HANDLER"] = "s";
+$config["MAX_IN_ROOM"] = 70;
+?>
